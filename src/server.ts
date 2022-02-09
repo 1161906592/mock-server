6f04e39d704e3b1d325e5af44b5546f8893bb48b
liuyang.0826 <liuyang.0826@bytedance.com>
2022-02-09 19:04:45 +0800

    feat(tips): add running tips

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,8 @@ const router = new KoaRouter();
 
 app.use(koaBody());
 app.use(cors());
-
 app.use(router.routes());
+
 glob(".mock/**/*.js", function (_, files) {
   files.forEach(async (file) => {
     const modules = (await import(`${process.cwd()}/${file}`)).default as MockModule[];
@@ -55,5 +55,5 @@ glob(".mock/**/*.js", function (_, files) {
 });
 
 app.listen(6789, () => {
-  console.log("mock server start: http://localhost:6789");
+  console.log("  > Mock-server running at: http://localhost:6789");
 });
